e2e5629d6b4e9ba944dc353c4b5d38f7192a103d
Abdumalik <axtrem13@gmail.com>
2019-11-20 18:14:26 +0500

    commit

---
+++ src/views/category.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+        <h1>Category</h1>
+        <ul>
+            <li v-for="category in categories"> {{ category }} </li>
+        </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+    components: {
+        asd: () => import('@/views/users')
+    },
+    data()  {
+        return {
+              categories: ['animals','birds','trees']
+        }
+    }  
+}
+</script>
---
+++ src/views/users.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+      <h1>Users</h1>
+      <ul>
+        <li v-for="(user, i) in users" :key="user.id" @click="changeName(user.name)" @dblclick="removeUser(i)">
+          {{user.id}} {{ user.name}} 
+          <input type="text" :value="user.name" :id="user.id" v-show="flag">
+        </li>
+        
+      </ul>
+      <hr>
+      <div>
+        <input type="text" v-model="index">
+        <button @click="searchUser">Search user</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data()  {
+        return {
+          flag: false,
+          users: [
+            {
+                name: 'Vasya',
+                id: 1
+            },
+            {
+                name: 'Petya',
+                id: 2
+            },
+            {
+                name: 'Lena',
+                id: 3
+            },
+            {
+                name: 'Katya',
+                id: 4
+            },
+            {
+                name: 'Vanya',
+                id: 5
+            },
+            {
+                name: 'Tolik',
+                id: 6
+            },
+          ],
+          index: ''
+        }
+    },
+    methods: {
+      alertName(e) {
+          alert(
+            e
+          )
+      },
+      searchUser() {
+        let username = '';
+          if (this.index){
+            for (var i = 0; i< this.users.length; i++) {
+              if (this.users[i].id <= this.index)
+               username += this.users[i].name+' '
+             } 
+          }
+          console.log(username)
+      },
+      removeUser(e){
+        this.users.splice(e, 1)
+      },
+      changeName(e){
+        this.flag = !this.flag
+      }
+    },
+
+    computed:{
+      
+    }
+}
+</script>
+
+<style>
+  ul,li {
+    display: block;
+    text-align: left;
+  }
+</style>
